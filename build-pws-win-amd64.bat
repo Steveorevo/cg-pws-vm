94fa834e1ff710f0e0211bf6ce1096339c050b83
Stephen J. Carnam <steve@steveorevo.com>
2023-05-18 23:56:30 -0700

    Create build-pws-win-amd64.bat

---
+++ build-pws-win-amd64.bat
@@ -0,0 +1,8 @@
+;;
+;; Virtuosoft build script for Code Garden PWS (Personal Web Server) Edition
+;; on Windows x86 64-bit compatible systems.
+;;
+
+;; Download and install QEMU for Windows
+curl -o qemu-w64-setup-20230424.exe https://qemu.weilnetz.de/w64/2023/qemu-w64-setup-20230424.exe
+qemu-w64-setup-20230424.exe /S
