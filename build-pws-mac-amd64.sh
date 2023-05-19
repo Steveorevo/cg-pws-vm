79d09c009d984a7416637fcfb9d13b805e593da1
Stephen J Carnam <steveorevo@gmail.com>
2023-05-19 00:33:11 -0700

    wip

--- build-pws-mac-amd64.sh
+++ build-pws-mac-amd64.sh
@@ -4,10 +4,37 @@
 # on macOS x86 64-bit compatible systems.
 #
 
+# Check if Xcode is installed
+if ! command -v xcode-select &> /dev/null; then
+    echo "Xcode is not installed. Installing..."
+    xcode-select --install
+else
+    echo "Xcode is already installed."
+fi
+
 # Check if Homebrew is installed
 if ! command -v brew &> /dev/null; then
     echo "Homebrew is not installed. Installing..."
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "Homebrew is already installed."
+fi
+
+# Check if QEMU is installed
+if ! brew list --formula | grep -q '^qemu$'; then
+    echo "QEMU is not installed. Installing..."
+    brew install --force qemu
+else
+    echo "QEMU is already installed."
+fi
+
+# Check if ISO file already exists
+ISO_URL="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.7.0-amd64-netinst.iso"
+ISO_FILENAME="debian-11.7.0-amd64-netinst.iso"
+if [ ! -f "$ISO_FILENAME" ]; then
+    echo "Downloading Debian ISO..."
+    curl -o "$ISO_FILENAME" "$ISO_URL"
+    echo "Download complete."
+else
+    echo "Debian ISO already downloaded."
 fi
--- build-pws-win-amd64.bat
+++ build-pws-win-amd64.bat
@@ -1,8 +1,28 @@
-;;
-;; Virtuosoft build script for Code Garden PWS (Personal Web Server) Edition
-;; on Windows x86 64-bit compatible systems.
-;;
+@echo off
+::
+:: Virtuosoft build script for Code Garden PWS (Personal Web Server) Edition
+:: on Windows x86 64-bit compatible systems.
+::
 
-;; Download and install QEMU for Windows
-curl -o qemu-w64-setup-20230424.exe https://qemu.weilnetz.de/w64/2023/qemu-w64-setup-20230424.exe
-qemu-w64-setup-20230424.exe /S
+:: Check if QEMU is installed
+where qemu-system-x86_64 >nul 2>&1
+if %errorlevel% neq 0 (
+    echo QEMU is not installed. Downloading and installing...
+    curl -o qemu-w64-setup-20230424.exe https://qemu.weilnetz.de/w64/2023/qemu-w64-setup-20230424.exe
+    qemu-w64-setup-20230424.exe /S
+    echo QEMU installation completed.
+) else (
+    echo QEMU is already installed.
+)
+
+:: Check if ISO file already exists
+set ISO_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.7.0-amd64-netinst.iso
+set ISO_FILENAME=debian-11.7.0-amd64-netinst.iso
+if not exist "%ISO_FILENAME%" (
+    echo Downloading Debian ISO...
+    curl -o "%ISO_FILENAME%" "%ISO_URL%"
+    echo Download complete.
+) else (
+    echo Debian ISO already downloaded.
+)
+
