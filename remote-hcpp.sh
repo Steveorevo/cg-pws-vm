02acd453b9cfce2bb4786befd3d9b9963f479a5b
Stephen J Carnam <steve@steveorevo.com>
2023-08-06 01:29:49 -0700

    wip

--- remote-hcpp.sh
+++ remote-hcpp.sh
@@ -64,14 +64,14 @@ touch "/usr/local/hestia/data/hcpp/installed/ghost"
 
 # Install HCPP CG-PWS
 cd /usr/local/hestia/plugins
-git clone --depth 1 --branch "v1.0.0-beta.5" https://github.com/virtuosoft-dev/hcpp-cg-pws.git cg-pws 2>/dev/null
+git clone --depth 1 --branch "v1.0.0-beta.6" https://github.com/virtuosoft-dev/hcpp-cg-pws.git cg-pws 2>/dev/null
 cd /usr/local/hestia/plugins/cg-pws
 ./install
 touch "/usr/local/hestia/data/hcpp/installed/cg-pws"
 
 # Install HCPP WebDAV
 cd /usr/local/hestia/plugins
-git clone --depth 1 --branch "v1.0.0-beta.1" https://github.com/virtuosoft-dev/hcpp-webdav.git webdav 2>/dev/null
+git clone --depth 1 --branch "v1.0.0-beta.2" https://github.com/virtuosoft-dev/hcpp-webdav.git webdav 2>/dev/null
 cd /usr/local/hestia/plugins/webdav
 ./install
 touch "/usr/local/hestia/data/hcpp/installed/webdav"
