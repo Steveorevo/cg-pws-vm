0c5de7b586a2c4f44f8d4866f4e309d70b379a01
Stephen J Carnam <steve@steveorevo.com>
2023-09-13 18:23:47 -0700

    wip

--- remote-hcpp.sh
+++ remote-hcpp.sh
@@ -78,7 +78,7 @@ touch "/usr/local/hestia/data/hcpp/installed/webdav"
 
 # Install HCPP Quickstart
 cd /usr/local/hestia/plugins
-git clone --depth 1 --branch "v1.0.0-beta.3" https://github.com/virtuosoft-dev/hcpp-quickstart.git quickstart 2>/dev/null
+git clone --depth 1 --branch "v1.0.0-beta.4" https://github.com/virtuosoft-dev/hcpp-quickstart.git quickstart 2>/dev/null
 cd /usr/local/hestia/plugins/quickstart
 ./install
 touch "/usr/local/hestia/data/hcpp/installed/quickstart"
