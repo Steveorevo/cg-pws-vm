54752b3ed50088d5647728902cef7e1d258868a9
Stephen J Carnam <steve@steveorevo.com>
2023-09-27 21:27:36 -0700

    wip

--- remote-hcpp.sh
+++ remote-hcpp.sh
@@ -13,7 +13,7 @@ sleep 1
 
 # Install HestiaCP Pluginable project
 cd /tmp
-git clone --depth 1 --branch "v1.0.0-beta.31" https://github.com/virtuosoft-dev/hestiacp-pluginable.git 2>/dev/null
+git clone --depth 1 --branch "v1.0.0-beta.32" https://github.com/virtuosoft-dev/hestiacp-pluginable.git 2>/dev/null
 mv hestiacp-pluginable/hooks /etc/hestiacp
 rm -rf hestiacp-pluginable-main
 /etc/hestiacp/hooks/post_install.sh
@@ -65,7 +65,7 @@ touch "/usr/local/hestia/data/hcpp/installed/ghost"
 
 # Install HCPP VitePress
 cd /usr/local/hestia/plugins
-git clone --depth 1 --branch "v1.0.0-beta.1" https://github.com/virtuosoft-dev/hcpp-vitepress.git vitepress 2>/dev/null
+git clone --depth 1 --branch "v1.0.0-beta.2" https://github.com/virtuosoft-dev/hcpp-vitepress.git vitepress 2>/dev/null
 cd /usr/local/hestia/plugins/vitepress
 ./install
 touch "/usr/local/hestia/data/hcpp/installed/vitepress"
