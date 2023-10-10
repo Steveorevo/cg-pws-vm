2675eda77441b75c0360ce686ba4990cd198c58b
Stephen J Carnam <steve@steveorevo.com>
2023-10-09 19:37:52 -0700

    wip

--- build-pws-win-amd64.bat
+++ build-pws-win-amd64.bat
@@ -68,17 +68,4 @@ qemu-system-x86_64 ^
         -net nic -net user,hostfwd=tcp::8022-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::8023-:8023 ^
         -drive if=virtio,format=qcow2,file=pws-amd64.img ^
         -device virtio-balloon-pci
-        REM -machine q35 -accel tcg ^
-        REM -machine q35,vmport=off -accel whpx,kernel-irqchip=off ^
-        REM -vga std ^
-        REM -nographic
-        REM -drive if=pflash,format=raw,file=efi_amd64.img,readonly=on ^
-        REM -drive if=pflash,format=raw,file=efi_amd64_vars.img,readonly=on ^
-        REM -device virtio-net-pci,netdev=net0 ^
-        REM -netdev user,id=net0,hostfwd=tcp::%sshPort%-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::%cpPort%-%cpPort%%samba% ^
-        REM -fsdev local,id=virtfs0,path="%appFolder%",security_model=mapped-xattr,fmode=0644,dmode=0755 ^
-        REM -device virtio-9p-2000,fsdev=virtfs0,mount_tag=appFolder ^
-        REM -nographic
-        REM -display default,show-cursor=on
-        REM Reference qemu-system-x86_64.exe -boot d -cdrom debian-12.0.0-amd64-netinst.iso -m 4G -hda pws-amd64.img -cpu qemu64-v1 -vga virtio -smp 4,sockets=1,cores=4,threads=1 -machine q35,vmport=off -accel whpx,kernel-irqchip=off
 cd ..
