552cc1aa39920cbbe1a17065b3ab7522a0fa8f48
Stephen J Carnam <steve@steveorevo.com>
2024-07-18 19:05:30 -1000

    pre-compress runtime

--- install-hcpp.sh
+++ install-hcpp.sh
@@ -30,11 +30,17 @@ echo "Now compressing resulting files for redistribution"
 if [ -f "build/devstia-amd64.img" ]; then
     echo "Found devstia-amd64.img, compressing with BIOS"
     cd ./build
+    qemu-img convert -O qcow2 -c devstia-amd64.img devstia-amd64-compressed.img
+    rm devstia-amd64.img
+    mv devstia-amd64-compressed.img devstia-amd64.img
     tar -cJf devstia-amd64.tar.xz devstia-amd64.img bios.img
 fi
 if [ -f "build/devstia-arm64.img" ]; then
     echo "Found devstia-arm64.img, compressing with EFI"
     cd ./build
+    qemu-img convert -O qcow2 -c devstia-arm64.img devstia-arm64-compressed.img
+    rm devstia-arm64.img
+    mv devstia-arm64-compressed.img devstia-arm64.img
     tar -cJf devstia-arm64.tar.xz devstia-arm64.img efi_arm64.img efi_arm64_vars.img
 fi
 echo "Done! Finished compression, exiting"
